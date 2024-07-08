f23dfa8619973eb97bf936f1a471bc2a67092445
Anton <63235458+Anton-Mochinsky@users.noreply.github.com>
2024-07-08 14:39:33 +0300

    Update models.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,28 +1,3 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     phone_number = models.CharField(max_length=15)
-#     name = models.CharField(max_length=100)
-#
-# class Token(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     token = models.CharField(max_length=255)
-#     expiry_date = models.DateTimeField()
-#
-# class Table(models.Model):
-#     capacity = models.IntegerField()
-#     is_reserved = models.BooleanField(default=False)
-#
-# class Reservation(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     table = models.ForeignKey(Table, on_delete=models.CASCADE)
-#     date = models.DateField()
-#     time = models.TimeField()
-#     status = models.CharField(max_length=20, default='active')
-#
-
 from django.db import models
 from django.contrib.auth.models import User
 
